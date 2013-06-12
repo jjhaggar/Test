7612df75063b73f351cfa7d09e41578f8dc6e41e
JJHaggar <jjhaggar@gmail.com>
2013-06-12 18:10:00 +0200

    Cambio a madgeargames.com

---
+++ JimVaders tutorial mod2/src/com/madgeargames/ninjatrials/GameLoopUpdateHandler.java
@@ -0,0 +1,20 @@
+package com.madgeargames.ninjatrials;
+
+import org.andengine.engine.handler.IUpdateHandler;
+
+public class GameLoopUpdateHandler implements IUpdateHandler{
+
+	@Override
+	public void onUpdate(float pSecondsElapsed) {
+		((GameScene)BaseActivity.getSharedInstance().mCurrentScene).moveShip();
+		((GameScene)BaseActivity.getSharedInstance().mCurrentScene).cleaner();
+		
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
